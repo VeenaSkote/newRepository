97e82939106b849dc85be3af3062999d3f892598
admin <admin@kubernetes.docker.internal>
2024-12-17 17:25:04 +0530

    Fist commit

---
+++ Selenium/src/test/java/package1/selenium2.java
@@ -0,0 +1,11 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class selenium2 
+{@Test
+	 public void method1()
+	 {
+		System.out.println("hiiiiiiiiBYeeee"); 
+	 }
+}
---
+++ Selenium/src/test/java/package1/selnium1.java
@@ -0,0 +1,11 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class selnium1 
+{@Test
+ public void method1()
+ {
+	System.out.println("hiiiiiiii"); 
+ }
+}
